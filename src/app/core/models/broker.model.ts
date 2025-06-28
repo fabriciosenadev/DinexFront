d07be3c1c7c91755d6ce8c25ceaeff60bbd5f182
Fabricio Sena <fabricio.sena@eaibrasil.com>
2025-06-28 14:36:30 -0300

    chore: remove toda codebase Angular para iniciar React do zero

--- src/app/core/models/broker.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Broker {
-  id: string;
-  name: string;
-  cnpj?: string;
-  website?: string;
-}
--- src/app/core/models/wallet.model.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Wallet {
-  id: string;
-  userId: string;
-  name: string;
-  description?: string;
-  defaultCurrency: string;
-}
